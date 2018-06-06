8f6f3d2fb05b595ff1920cf4d18ab8af06f1c92a
MFC <liyanxi07@gmail.com>
2018-06-07 02:09:34 +0800

    add: validator update and dev user/signals.py

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -9,6 +9,7 @@ from datetime import datetime, timedelta
 
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from MxShop.settings import REGEX_MOBILE
 from users.models import VerifyCode
@@ -43,15 +44,41 @@ class SmsSerializer(serializers.Serializer):
 
 
 class UserRegSerializer(serializers.ModelSerializer):
-    code = serializers.CharField(required=True, max_length=4, min_length=4,
+    code = serializers.CharField(required=True, max_length=4, write_only=True, min_length=4, label="验证码",
                                  error_messages={
                                      'blank': '请输入验证码',
                                      'required': '请输入验证码',
                                      'max_length': '验证码格式错误',
                                      'minx_length': '验证码格式错误',
                                  }, help_text='验证码')   # 添加的字段
+    username = serializers.CharField(label="用户名", help_text="用户名", required=True, allow_blank=False,
+                                     validators=[UniqueValidator(queryset=User.objects.all(), message="用户已经存在")])
+    password = serializers.CharField(
+        label="密码", write_only=True,
+        style={'input_type': 'password'}
+    )
+
+    def create(self, validated_data):
+        """
+        重载create method  另一种方法是利用django信号量Signals的机制
+        :param validated_data:
+        :return:
+        """
+        user = super(UserRegSerializer, self).create(validated_data=validated_data)
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
 
     def validate_code(self, code):
+        # 如果使用get，需要try except
+        # try:
+        #     verify_records = VerifyCode.objects.get(mobile=self.initial_data["username"]).order_by("-add_time")
+        # except VerifyCode.DoesNotExist as e:
+        #     # 最好加上错误日志打印
+        #     pass
+        # except VerifyCode.MultipleObjectsReturned as e:
+        #     pass
+
         # 验证验证码是否存在于数据库
         verify_records = VerifyCode.objects.filter(mobile=self.initial_data["username"]).order_by("-add_time")   # initial_data, from front_end post
 
@@ -59,8 +86,8 @@ class UserRegSerializer(serializers.ModelSerializer):
             last_record = verify_records[0]
 
             five_minute_ago = datetime.now() - timedelta(hours=0, minutes=5, seconds=0)   # 有效期5分钟
-            # 减去5mins (five_minute_ago) > add_time说明还没有超过5mins, 即没过期
-            if five_minute_ago < last_record.add_time:
+
+            if five_minute_ago > last_record.add_time:
                 raise serializers.ValidationError("验证码过期")
 
             if last_record.code != code:
@@ -77,7 +104,7 @@ class UserRegSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("username", "code", "mobile")       # UserProfile继承的是django自带的字段
+        fields = ("username", "mobile", "code", "password")       # UserProfile继承的是django自带的字段
 
 
 
