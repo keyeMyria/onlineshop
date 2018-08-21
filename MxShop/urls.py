b1be6110864cc62117fae8ba5a03f282303f5be7
yanxi <testerlyx@foxmail.com>
2018-08-21 23:11:05 +0800

    add: add django admin for practice.

--- MxShop/urls.py
+++ MxShop/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-# from django.contrib import admin
+from django.contrib import admin
 from django.views.static import serve
 
 import xadmin
@@ -59,7 +59,11 @@ router.register(r'users', UserViewset, base_name="users")
 # })
 
 urlpatterns = [
+
+    url(r'^admin/', admin.site.urls),
+
     url(r'^xadmin/', xadmin.site.urls),
+
     url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
 
     # goods list page
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from users.models import VerifyCode
 
 # Register your models here.
+
+
+class VerifyCodeAdmin(admin.ModelAdmin):
+    list_display = ['code', 'mobile', "add_time"]
+    search_fields = ['mobile', 'code']
+
+
+admin.site.register(VerifyCode, VerifyCodeAdmin)
